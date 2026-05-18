6d9f0a3c8dda6eb8d29febbf1edeaa62bfdb93ba
cause-dev <cause.dev@proton.me>
2026-05-18 12:13:14 +0500

    Add dashboard page

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,9 @@
+const DashboardPage = () => {
+  return (
+    <div className="flex w-full items-center justify-center">
+      <h1>Dashboard</h1>
+    </div>
+  );
+};
+
+export default DashboardPage;
