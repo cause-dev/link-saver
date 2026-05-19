0cccad02d0652567a16488c112d8b6a3a6fd71d2
cause-dev <cause.dev@proton.me>
2026-05-19 11:03:12 +0500

    Add logged-in user's name and avatar on header

--- components/main-header.tsx
+++ components/main-header.tsx
@@ -1,6 +1,20 @@
+"use client";
+
 import Link from "next/link";
+import Image from "next/image";
+import { useSession } from "@/lib/auth-client";
 
 const MainHeader = () => {
+  const { error, isPending, data } = useSession();
+
+  if (isPending) {
+    return <p>Loading ...</p>;
+  }
+
+  if (error) {
+    return <p>Something has gone wrong.</p>;
+  }
+
   return (
     <div className="flex h-20 w-full items-center justify-between bg-surface-2 px-10 text-fg">
       <h2 className="text-2xl font-bold">
@@ -20,9 +34,25 @@ const MainHeader = () => {
         </ul>
       </nav>
       <div>
-        <button className="flex h-10 w-24 cursor-pointer items-center justify-center rounded-full bg-primary font-bold transition-[3s] hover:bg-primary-hover">
-          Login
-        </button>
+        {!data?.user ? (
+          <Link
+            href="/login"
+            className="flex h-10 w-24 cursor-pointer items-center justify-center rounded-full bg-primary font-bold transition-[3s] hover:bg-primary-hover"
+          >
+            Sign In
+          </Link>
+        ) : (
+          <div className="flex flex-col items-center justify-center gap-1">
+            <Image
+              src={data.user.image ?? "/cat.png"}
+              width={30}
+              height={30}
+              alt={data.user.name ?? "user"}
+              className="rounded-full"
+            />
+            <span className="text-xs">{data.user.name}</span>
+          </div>
+        )}
       </div>
     </div>
   );
