76cde11639bc2d4047fd96e77fa22da2efdd18f2
cause-dev <cause.dev@proton.me>
2026-05-19 11:33:21 +0500

    Add a logout dropdown

--- components/main-header.tsx
+++ components/main-header.tsx
@@ -1,11 +1,27 @@
 "use client";
 
+import { useEffect, useRef, useState } from "react";
 import Link from "next/link";
 import Image from "next/image";
-import { useSession } from "@/lib/auth-client";
+import { useSession, signOut } from "@/lib/auth-client";
 
 const MainHeader = () => {
   const { error, isPending, data } = useSession();
+  const [openDropdown, setOpenDropdown] = useState(false);
+  const ref = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      if (ref.current && !ref.current.contains(event.target as Node)) {
+        setOpenDropdown(false);
+      }
+    };
+    document.addEventListener("mousedown", handleClickOutside);
+
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []);
 
   if (isPending) {
     return <p>Loading ...</p>;
@@ -33,16 +49,19 @@ const MainHeader = () => {
           </li>
         </ul>
       </nav>
-      <div>
+      <div ref={ref} className="relative">
         {!data?.user ? (
           <Link
-            href="/login"
+            href="/sign-in"
             className="flex h-10 w-24 cursor-pointer items-center justify-center rounded-full bg-primary font-bold transition-[3s] hover:bg-primary-hover"
           >
             Sign In
           </Link>
         ) : (
-          <div className="flex flex-col items-center justify-center gap-1">
+          <button
+            onClick={() => setOpenDropdown(!openDropdown)}
+            className="flex cursor-pointer flex-col items-center justify-center gap-1"
+          >
             <Image
               src={data.user.image ?? "/cat.png"}
               width={30}
@@ -51,6 +70,16 @@ const MainHeader = () => {
               className="rounded-full"
             />
             <span className="text-xs">{data.user.name}</span>
+          </button>
+        )}
+        {openDropdown && (
+          <div className="absolute top-17 right-0.5 flex items-center justify-center rounded-xl bg-active px-5 py-3">
+            <button
+              onClick={() => signOut()}
+              className="cursor-pointer text-red-400"
+            >
+              Logout
+            </button>
           </div>
         )}
       </div>
