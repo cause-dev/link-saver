35194c1148e69ded1dba4b3cb577387a9b2b78c6
cause-dev <cause.dev@proton.me>
2026-05-20 13:20:52 +0500

    Send link data to backend using submit form

--- app/api/links/route.ts
+++ app/api/links/route.ts
@@ -1,21 +1,56 @@
 import prisma from "@/lib/prisma";
+import { auth } from "@/lib/auth";
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
 
-export async function POST(req: Request) {
+export async function POST(req: NextRequest) {
+  const session = await auth.api.getSession({
+    headers: req.headers,
+  });
+
+  if (!session) {
+    return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+  }
   try {
     const body = await req.json();
 
+    const { title, url, tags } = body;
+
+    if (!url) {
+      return NextResponse.json({ error: "URL is required" }, { status: 400 });
+    }
+
     const link = await prisma.link.create({
       data: {
-        title: body.title,
-        url: body.url,
-        tags: body.tags,
+        title,
+        url,
+        userId: session.user.id,
+        tags: {
+          connectOrCreate: tags.map((tag: string) => ({
+            where: { name: tag },
+            create: { name: tag },
+          })),
+        },
+      },
+      include: {
+        tags: true,
       },
     });
 
-    return Response.json(link, { status: 201 });
-  } catch (error) {
-    return Response.json(
-      { error: "Failed to create user", message: error },
+    return NextResponse.json(link);
+  } catch (error: any) {
+    console.error("DEBUG - Prisma Error:", error);
+    if (error.code === "P2002") {
+      return NextResponse.json(
+        { error: "This URL has already been saved." },
+        { status: 400 },
+      );
+    }
+    return NextResponse.json(
+      {
+        error: "Failed to create link",
+        message: error.message || "Unknown error",
+      },
       { status: 500 },
     );
   }
