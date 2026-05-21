ddb5973bcfdb1daab2c7c917e4a2acea885fca33
cause-dev <cause.dev@proton.me>
2026-05-21 16:45:45 +0500

    Show fetch links on dashboard

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -16,10 +16,21 @@ const DashboardPage = async () => {
     orderBy: { createdAt: "desc" },
   });
 
-  console.log(links);
   return (
-    <div className="flex w-full items-center justify-center">
-      <h1>Dashboard</h1>
+    <div className="grid w-full items-center justify-center gap-4">
+      {links.map((link) => {
+        return (
+          <a
+            key={link.id}
+            href={link.url}
+            target="_blank"
+            className="flex gap-3 rounded-3xl bg-active px-5 py-2"
+          >
+            <h3 className="font-bold">{link.title}</h3>
+            <p>{link.url}</p>
+          </a>
+        );
+      })}
     </div>
   );
 };
